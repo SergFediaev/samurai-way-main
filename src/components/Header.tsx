1a04ca761544634f9024c68b9a8f7b987984690e
SergFediaev <sergfediaev@gmail.com>
2024-02-15 11:51:33 +0300

    Путь самурая 1.0: 14. Уроки React JS (css-модули, css-modules).

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import logo from "../logo.jpg";
+import React from 'react'
+import logo from '../logo.jpg'
+import classes from './Header.module.css'
 
-export const Header = () => <header className='header'>
+export const Header = () => <header className={classes.header}>
     <img
         // src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1448711260.1707235200&semt=ais"
         src={logo}
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from 'react'
+import classes from './NavBar.module.css'
 
-export const NavBar = () => <nav className='nav'>
-    <div><a href='#'>Profile</a></div>
-    <div><a href='#'>Messages</a></div>
-    <div><a href='#'>New</a></div>
-    <div><a href='#'>Music</a></div>
-    <div><a href='#'>Settings</a></div>
+console.log(classes)
+
+const itemStyle = classes.item
+const activeStyle = classes.active
+const combinedStyles = itemStyle + ' ' + activeStyle
+const doubleStyles = `${classes.item} ${classes.active}`
+
+export const NavBar = () => <nav className={classes.nav}>
+    <div className={classes.item}><a>Profile</a></div>
+    <div className={combinedStyles}><a>Messages</a></div>
+    <div className={classes.item}><a>New</a></div>
+    <div className={doubleStyles}><a>Music</a></div>
+    <div className={classes.item}><a>Settings</a></div>
 </nav>
