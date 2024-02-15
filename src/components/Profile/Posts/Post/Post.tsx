7914b57e0eed7d7cff48ae00fe35392842d3c752
SergFediaev <sergfediaev@gmail.com>
2024-02-15 14:06:23 +0300

    Путь самурая 1.0: 16. Уроки React JS (props - важнейшая тема) - react курсы бесплатно + домашка.

--- src/components/Profile/Posts/Post/Post.tsx
+++ src/components/Profile/Posts/Post/Post.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import classes from './Post.module.css'
 
-export const Post = () => {
+type PostPropsType = {
+    message: string
+    likesCount: number
+}
+
+export const Post = (props: PostPropsType) => {
+    // debugger
+    // console.log(props)
+
     return <div className={classes.item}><img
         src="https://asset.kompas.com/crops/-QSHMGMmRvrDcDZeZbRh0wrk4NM=/0x81:466x391/750x500/data/photo/2023/11/09/654c73dbe7559.jpg"
-        alt="Avatar"/>Post 1xj
+        alt="Avatar"/>{props.message}
         <div><span>Like</span></div>
+        <div>Likes: {props.likesCount}</div>
     </div>
 }
