febc9b6a4ffe173c46d7afa8ba9f5a3e12911aec
SergFediaev <sergfediaev@gmail.com>
2024-02-21 16:09:57 +0300

    Путь самурая 1.0: 18. Уроки React JS (страница Dialogs) - react курсы бесплатно.

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import './App.css'
 import {Header} from './components/Header/Header'
 import {NavBar} from './components/NavBar/NavBar'
-import {Profile} from './components/Profile/Profile'
+import {Dialogs} from './components/Dialogs/Dialogs'
 
 export const App = () => <div className="app-wrapper">
     <Header/>
     <NavBar/>
-    <Profile/>
+    <div className="app-wrapper-content">
+        <Dialogs/>
+        {/*<Profile/>*/}
+    </div>
 </div>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import classes from './Profile.module.css'
 import {Posts} from './Posts/Posts'
 
-export const Profile = () => <div className={classes.content}>
+export const Profile = () => <div>
     <div>
         <img
             src="https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630"
