58b54ddd50eb2d0c1c51538459a8050b14727a12
SergFediaev <sergfediaev@gmail.com>
2024-02-26 20:32:28 +0300

    Путь самурая 1.0, урок + домашка: 19. Уроки React JS (route, browser-router, маршрутизация) - react курсы бесплатно.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,26 @@ import './App.css'
 import {Header} from './components/Header/Header'
 import {NavBar} from './components/NavBar/NavBar'
 import {Dialogs} from './components/Dialogs/Dialogs'
+import {BrowserRouter, Route} from 'react-router-dom'
+import {Profile} from './components/Profile/Profile'
+import {New} from './components/New/New'
+import {Music} from './components/Music/Music'
+import {Settings} from './components/Settings/Settings'
 
-export const App = () => <div className="app-wrapper">
-    <Header/>
-    <NavBar/>
-    <div className="app-wrapper-content">
-        <Dialogs/>
-        {/*<Profile/>*/}
-    </div>
-</div>
+export const App = () => {
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <NavBar/>
+                <div className="app-wrapper-content">
+                    <Route path="/profile" component={Profile}/>
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/new" component={New}/>
+                    <Route path="/music" component={Music}/>
+                    <Route path="/settings" component={Settings}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
+}
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import classes from './NavBar.module.css'
 
-console.log(classes)
-
-const itemStyle = classes.item
-const activeStyle = classes.active
-const combinedStyles = itemStyle + ' ' + activeStyle
-const doubleStyles = `${classes.item} ${classes.active}`
-
 export const NavBar = () => <nav className={classes.nav}>
-    <div className={classes.item}><a>Profile</a></div>
-    <div className={combinedStyles}><a>Messages</a></div>
-    <div className={classes.item}><a>New</a></div>
-    <div className={doubleStyles}><a>Music</a></div>
-    <div className={classes.item}><a>Settings</a></div>
+    <div className={classes.item}><a href="/profile">Profile</a></div>
+    <div className={classes.item}><a href="/dialogs">Messages</a></div>
+    <div className={classes.item}><a href="/new">New</a></div>
+    <div className={classes.item}><a href="/music">Music</a></div>
+    <div className={classes.item}><a href="/settings">Settings</a></div>
 </nav>
