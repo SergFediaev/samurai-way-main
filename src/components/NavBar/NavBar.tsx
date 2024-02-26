d7181bbf72818d06abc1e6377c68c335a4ca1613
SergFediaev <sergfediaev@gmail.com>
2024-02-26 21:25:43 +0300

    Путь самурая 1.0, урок: 20. Уроки React JS (NavLink) - react курсы бесплатно.

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import classes from './NavBar.module.css'
+import {NavLink} from 'react-router-dom'
 
 export const NavBar = () => <nav className={classes.nav}>
-    <div className={classes.item}><a href="/profile">Profile</a></div>
-    <div className={classes.item}><a href="/dialogs">Messages</a></div>
-    <div className={classes.item}><a href="/new">New</a></div>
-    <div className={classes.item}><a href="/music">Music</a></div>
-    <div className={classes.item}><a href="/settings">Settings</a></div>
+    <div className={classes.item}><NavLink to="/profile" activeClassName={classes.activeLink}>Profile</NavLink></div>
+    <div className={classes.item}><NavLink to="/dialogs" activeClassName={classes.activeLink}>Messages</NavLink></div>
+    <div className={classes.item}><NavLink to="/new" activeClassName={classes.activeLink}>New</NavLink></div>
+    <div className={classes.item}><NavLink to="/music" activeClassName={classes.activeLink}>Music</NavLink></div>
+    <div className={classes.item}><NavLink to="/settings" activeClassName={classes.activeLink}>Settings</NavLink></div>
 </nav>
