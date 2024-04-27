47e976d03465e84bdc345c644fc76e18d08084dd
SergFediaev <sergfediaev@gmail.com>
2024-04-28 01:40:00 +0300

    Путь самурая 1.0, урок: 52 - React JS практика - Side Effect.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,13 +5,17 @@ import axios from 'axios'
 import userPhoto from '../../assets/images/user.png'
 
 export const Users = (props: UsersPagePropsType) => {
-    if (props.users.length === 0) {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+    const getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
+
     return <div>
+        <button onClick={getUsers}>Get users</button>
         {props.users.map(user => <div key={user.id}>
             <span>
                 <div>
